51bd23ed5fd7a1242c2062773b01201d6da39ce4
escaper <assaoui54@gmail.com>
2023-12-27 17:38:14 +0100

    fixd the Werkzeug package so that flask can run

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,12 @@
-# Installs flask package
-package { 'flask':
-    ensure   => '2.1.0',
-    provider => 'pip3',
+#!/usr/bin/pup
+# Install an especific version of flask (2.1.0)
+package {
+    'Werkzeug':
+  ensure   => '2.2.2',
+  provider => 'pip3',
+}
+package {
+    'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3'
 }
