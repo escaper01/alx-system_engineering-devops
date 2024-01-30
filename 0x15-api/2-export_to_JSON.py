a65ba750fba9e1db672406c3890736366f7823d0
escaper <assaoui54@gmail.com>
2024-01-30 01:12:37 +0100

    feat: 0x15 api

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Exports to-do list information for a given employee ID to JSON format."""
+import json
+import requests
+import sys
+
+if __name__ == "__main__":
+    user_id = sys.argv[1]
+    user = requests.get("https://jsonplaceholder.typicode.com/users/{}".format(user_id)).json()
+    username = user.get("username")
+    todos = requests.get("https://jsonplaceholder.typicode.com/todos", params={"userId": user_id}).json()
+
+    with open("{}.json".format(user_id), "w") as jsonfile:
+        json.dump({user_id: [{
+                "task": t.get("title"),
+                "completed": t.get("completed"),
+                "username": username
+            } for t in todos]}, jsonfile)
