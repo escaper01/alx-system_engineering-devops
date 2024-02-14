d61cbcf7a3e9eb96a209c6d8b0343cb156649132
escaper <assaoui54@gmail.com>
2024-02-14 14:54:28 +0100

    another fix

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,14 +7,13 @@ import requests
 
 
 def number_of_subscribers(subreddit):
-    """
-    Queries the Reddit API and returns the number of total subscribers for a
-    given subreddit.
-    """
-    url = 'http://www.reddit.com/r/{}/about.json'.format(subreddit)
-    headers = {'User-Agent': 'my own custom header'}
-    response = requests.get(url, headers=headers)
-    if (not response.ok):
+    """Return the total number of subscribers on a given subreddit."""
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+    }
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 404:
         return 0
-    subscriber_count = response.json().get('data').get('subscribers')
-    return subscriber_count
+    results = response.json().get("data")
+    return results.get("subscribers")
