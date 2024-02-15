90cdbaa5ee977d386d9e997e4f703dd55ce8bcba
escaper <assaoui54@gmail.com>
2024-02-15 14:13:38 +0100

    -_-

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,24 +1,12 @@
 #!/usr/bin/python3
-""" Python Script return the number of subscribers """
+"""Returns the number of subscribers of a subreddit"""
 import requests
 
 
-
-
-
 def number_of_subscribers(subreddit):
-    """
-        number_of_subscribers Function Scrap Subscribers Number
-            subrredit - channel to check subscribers number
-            return - subscribers number
-            return - 0 if subreddit not found
-    """
-    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
-    response = requests.get(url, headers={'user-agent': 'CustomHeader'}, allow_redirects=False)
-
-    if not response.status_code == 200:
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {'User-Agent': 'Mozilla/5.0 (Linux x86_64) Edge109.0'}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code != 200:
         return 0
-    subscribers = response.json().get('data').get('subscribers')
-    if subscribers is not None:
-        return subscribers
-    return 0
+    return response.json()['data']['subscribers']
