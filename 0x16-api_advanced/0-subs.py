9778f493eeb34e4e0270526378ff39e7b43691a0
escaper <assaoui54@gmail.com>
2024-02-15 14:16:23 +0100

    @_@

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,12 +1,21 @@
 #!/usr/bin/python3
-"""Returns the number of subscribers of a subreddit"""
+"""Queries the reddit api and returns number of suscribers"""
+
 import requests
 
 
 def number_of_subscribers(subreddit):
+    """returns the number of subscribers to the subreddit"""
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {'User-Agent': 'Mozilla/5.0 (Linux x86_64) Edge109.0'}
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code != 200:
+    sub_info = requests.get(
+        url,
+        headers={"User-Agent": "My-User-Agent"},
+        allow_redirects=False,
+    )
+
+    if sub_info.status_code != 200:
         return 0
-    return response.json()['data']['subscribers']
+    data = sub_info.json().get("data")
+    if data is None:
+        return 0
+    return data.get("subscribers", 0)
